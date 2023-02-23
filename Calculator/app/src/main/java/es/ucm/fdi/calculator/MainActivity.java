db1f99db583a6f51fde52a52232ca60edac7a827
erosguerrero <94256335+erosguerrero@users.noreply.github.com>
2023-02-23 13:36:31 +0100

    backEnd

--- Calculator/app/src/main/java/es/ucm/fdi/calculator/MainActivity.java
+++ Calculator/app/src/main/java/es/ucm/fdi/calculator/MainActivity.java
@@ -5,40 +5,44 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     private Calculator calculadora = new Calculator();
     private EditText x,y;
-    private Button operate;
+    private TextView operate;
     private String op;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         this.op = "11+112+3+4+5+1";
+    }
+    public void operates(View view){
+        Log.i("MainActivity", "LOLOLOLdentro de onClickListener");
 
-        operate = findViewById(R.id.equalsButton);
-        Log.i("MainActivity", "dentro de onCreate");
-            operate.setOnClickListener(view -> {
-                Log.i("MainActivity", "dentro de onClickListener");
+        if (!this.op.equals("")) {
 
-                if (!this.op.equals("")) {
+            Double ant = 0.0;
+            for (String op : op.split("\\+")) {
+                ant = calculadora.suma(ant,Double.parseDouble(op));
+            }
 
-                    Double ant = 0.0;
-                    for (String op : op.split("\\+")) {
-                        ant = calculadora.suma(ant,Double.parseDouble(op));
-                    }
+            Intent intent = new Intent(this, CalculatorResultActivity.class);
+            intent.putExtra("resultado", ant);
+            startActivity(intent);
+        } else {
+            Log.e("MainActivity", "Alguno de los operandos está vacío");
+        }
+    }
 
-                    Intent intent = new Intent(this, CalculatorResultActivity.class);
-                    intent.putExtra("resultado", ant);
-                    startActivity(intent);
-                } else {
-                    Log.e("MainActivity", "Alguno de los operandos está vacío");
-                }
-
-            });
+    public void buttonToOperate(View view){
+        TextView texto = (TextView) view;
+        this.op+=texto.getText().toString();
     }
 }
